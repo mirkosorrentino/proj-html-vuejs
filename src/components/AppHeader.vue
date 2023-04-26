3c66e34dfe09d1d0a5d35c64dbf2462c5bf86efc
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-26 12:37:39 +0200

    first commit

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,26 @@
+<script>
+import AppJumbotron from "./AppJumbotron.vue"
+
+export default {
+    name: "AppHeader",
+    components: {
+        AppJumbotron
+    },
+
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
+
+<template>
+    <header></header>
+    <AppJumbotron/>
+</template>
+
+<style scoped lang="scss">
+@use "../styles/partials/mixins.scss" as *;
+@use "../styles/partials/variables.scss" as *;
+</style>
---
+++ src/components/AppJumbotron.vue
@@ -0,0 +1,17 @@
+<script>
+export default {
+    name: "AppJumbotron",
+    data(){
+        return{
+            
+        }
+    }
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use "../styles/partials/mixins.scss" as *;
+@use "../styles/partials/variables.scss" as *;
+</style>
