64968d1625d26aa9b5dbce40d8ef90be07e2c894
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-27 17:09:14 +0200

    Add: success data section

--- src/components/mainComponents/SuccessData.vue
+++ src/components/mainComponents/SuccessData.vue
@@ -3,7 +3,24 @@ export default {
     name: "SuccessData",
     data(){
         return{
-
+            successDatas: [
+                {
+                    number: "1.926",
+                    detail: "Finished lessons"
+                },
+                {
+                    number: "3092+",
+                    detail: "Enrolled learners"
+                },
+                {
+                    number: "200",
+                    detail: "Online instructors"
+                },
+                {
+                    number: "100%",
+                    detail: "Satisfaction rate"
+                },
+            ]
         }
     }
 }
@@ -11,7 +28,12 @@ export default {
 
 <template>
     <div class="ms_success-data ms_container">
-        Success Data
+        <div class="grid d-flex text-center">
+            <div class="col-3" v-for="data in successDatas">
+                <div class="ms_number-data"><h2>{{ data.number }}</h2> </div>
+                <div class="ms_detail-data"> {{ data.detail }} </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,9 +42,17 @@ export default {
 @use "../../styles/partials/variables.scss" as *;
 
 .ms_success-data {
-    // debug
-    height: 200px;
-    background-color: lightslategrey;
-    margin-top: 20px;
+    padding: 4rem 0;
+    
+    h2 {
+        font-weight: 700;
+        color: $green-btns;
+    }
+    .ms_detail-data {
+        text-transform: uppercase;
+        color: $blue-text;
+        font-weight: 400;
+        font-size: .8rem;
+    }
 }
 </style>
