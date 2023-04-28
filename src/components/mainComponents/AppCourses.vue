6573351814922827f4e756ff3da649a4948cde60
mirkosorrentino <mirkosorrentino6454@gmail.com>
2023-04-28 11:44:29 +0200

    Add: static AppCourses

--- src/components/mainComponents/AppCourses.vue
+++ src/components/mainComponents/AppCourses.vue
@@ -1,5 +1,5 @@
 <script>
-export default {
+export default{
     name: "AppCourses",
     data(){
         return{
@@ -11,7 +11,112 @@ export default {
 
 <template>
     <div class="ms_courses ms_container">
-        Courses
+        <div class="ms_subtitle text-center">Choose where you'd like to begin</div>
+        <h2 class="text-center pb-3 pt-2">Latest Featured <span class="ms_green-title">Courses</span></h2>
+        <div class="grid d-flex flex-wrap">
+            <!-- first card -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- second photo -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- third card -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- fourth card -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- fifth card -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <!-- sixth card -->
+            <div class="col-4 p-3">
+                <div class="ms_courses-card">
+                    <img src="../../assets/images/course-02-480x298.jpg" alt="" class="pb-3">
+                    <div class="ms_container">
+                        <div class="ms_course-teacher">
+                            <img src="../../assets/images/73ee246daf47502812ccefc84bf02898 (1).jpeg" alt="" class="ms_teacher-photo">
+                            <span class="ms_teacher-lessons">Blanche Fields</span>
+                        </div>
+                        <div class="ms_courses-name">Learning to Write as a Professional Author</div>
+                        <div class="ms_course-datas">
+                            <span class="ms_teacher-lessons">20 lessons</span>
+                            <span class="ms_teacher-lessons">50 students</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
@@ -20,9 +125,22 @@ export default {
 @use "../../styles/partials/variables.scss" as *;
 
 .ms_courses {
-    // debug
-    height: 200px;
-    background-color: greenyellow;
-    margin-top: 20px;
+    padding-top: 3rem;
+    .ms_course-teacher {
+        .ms_teacher-photo {
+            width: 10%;
+            border-radius: 50%;
+            padding-right: 7px;
+        }
+    }
+    .ms_courses-name {
+        font-weight: 500;
+        padding: .6rem 0 1rem;
+    }
+}
+.ms_teacher-lessons {
+    font-size: .8rem;
+    color: gray;
+    padding-right: 2rem;
 }
 </style>
